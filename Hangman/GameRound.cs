1f38280ed2038a020eba14ea24e048bb6d4fc8b6
DESKTOP-099RT6O\wilkqqq <matesz.brodocz@gmail.com>
2020-11-20 22:03:15 +0100

    Update game round to quess whole word fix checking is letter in word

--- Hangman/GameRound.cs
+++ Hangman/GameRound.cs
@@ -1,85 +1,140 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Hangman
 {
     class GameRound
     {
+        string secretWord;
         public void PlayGameRound()
         {
             var secredWordGenerator = new SecredWordCountryGenerator();
             Console.Title = ("HangMan Game");
             var countryWithCapitalCity = secredWordGenerator.GetSecredWord();
-            string secretWord = countryWithCapitalCity.CapitalCity;
+            secretWord = countryWithCapitalCity.CapitalCity;
             List<string> letterGuessed = new List<string>();
+            List<string> letterGuessedButWrong = new List<string>();
             int live = 5;
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("Welcome To HangMan Game");
             Console.WriteLine("hello");
             Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("Guess for a {0} Letter Word  ", secretWord.Length);
+            Console.WriteLine($"Guess for a {secretWord.Length} letter word that is a capital city");
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("You Have {0} Live", live);
-           
+            Console.WriteLine($"You Have {live} Live");
 
-            PrintWordBoard(secretWord, letterGuessed);
+            PrintWordBoard(secretWord, letterGuessed, letterGuessedButWrong);
 
             while (live > 0)
             {
-                Console.ForegroundColor = ConsoleColor.Green;
-                string input = Console.ReadLine();
-                if (IsAlreadyGuessedBefore(letterGuessed, input))
+                string wordOrLetter = null;
+                do
                 {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("You Entered Letter [{0}] already", input);                              
-                    Console.ForegroundColor = ConsoleColor.DarkBlue;
-                    Console.WriteLine("Try a Different Word");
-                    PrintLeftAlphabet(input);
-                    continue;
-                }
+                    Console.WriteLine("Do you want to guess a letter or a word? l/w");
+                    wordOrLetter = Console.ReadLine();
 
-                letterGuessed.Add(input);                                        
-                if (IsWordToGuess(secretWord, letterGuessed))
-                {
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.WriteLine(secretWord);
-                    Console.WriteLine("Congratulations");
+                    if (wordOrLetter != "l" && wordOrLetter != "w")
+                    {
+                        Console.WriteLine("I do not understand.");
+                        continue;
+                    }
                     break;
-                }
+                } while (true);
 
-                if (IsLetterInWordToGuess(secretWord, input))
+                if (wordOrLetter == "l")
                 {
-                    Console.ForegroundColor = ConsoleColor.Magenta;
-                    Console.WriteLine("Nice Entry");
-                    PrintWordBoard(secretWord, letterGuessed);
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Letter Not in My Word ");
-                    live -= 1;
-                    Console.ForegroundColor = ConsoleColor.Blue;
-                    Console.WriteLine("You Have {0} Live", live);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Provide a letter");
+                    string input = Console.ReadLine().ToLower();
 
-                    PrintWordBoard(secretWord, letterGuessed);
+                    if (IsAlreadyGuessedBefore(letterGuessed, input))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"You entered letter [{input}] already");
+                        Console.ForegroundColor = ConsoleColor.DarkBlue;
+                        Console.WriteLine("Try a different letter");
+                        continue;
+                    }
+
+                    letterGuessed.Add(input);
+                    if (IsWordToGuess(secretWord, letterGuessed))
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine(secretWord);
+                        Console.WriteLine("Congratulations");
+                        break;
+                    }
+
+                    if (IsLetterInWordToGuess(secretWord, input))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        Console.WriteLine("Nice Entry");
+                        PrintWordBoard(secretWord, letterGuessed, letterGuessedButWrong);
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Letter not in my word ");
+                        live -= 1;
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.WriteLine($"You have {live} live");
+                        letterGuessedButWrong.Add(input);
+
+                        PrintWordBoard(secretWord, letterGuessed, letterGuessedButWrong);
+                    }
+                }
+                else if (wordOrLetter == "w")
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Provide a word");
+                    string input = Console.ReadLine().ToLower();
+
+                    if (input.ToLower() == secretWord.ToLower())
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine(secretWord);
+                        Console.WriteLine("Congratulations");
+                        break;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("This is not a word to guess :(");
+                        live -= 2;
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.WriteLine($"You have {live} live");
+
+                        PrintWordBoard(secretWord, letterGuessed, letterGuessedButWrong);
+                    }
                 }
 
                 Console.WriteLine();
-                if (live == 0)
+                if (live <= 0)
                 {
                     Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("Game Over \nMy Secret Word is [ {0} ]", secretWord);
+                    Console.WriteLine($"Game Over \nmy secret word is [ {secretWord} ]");
                     break;
                 }
-                
+
+                Console.WriteLine();
+                if (live <= 2)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"The capital of {countryWithCapitalCity.Country}");
+                }
             }
         }
 
-        private static void PrintWordBoard(string secretword, List<string> letterGuessed)
+        private static void PrintWordBoard(string secretword, List<string> letterGuessed, List<string> letterGuessedButWrong)
         {
             Console.ForegroundColor = ConsoleColor.DarkYellow;
             Console.WriteLine(Isletter(secretword, letterGuessed));
+            Console.WriteLine($"Wrong letters: ");
+            foreach (var letter in letterGuessedButWrong)
+            {
+                Console.Write($" {letter} ");
+            }
+            Console.WriteLine();
         }
 
         private static bool IsAlreadyGuessedBefore(List<string> letterGuessed, string input)
@@ -97,14 +152,14 @@ namespace Hangman
             bool word = false;
             for (int i = 0; i < secreword.Length; i++)
             {
-                string c = Convert.ToString(secreword[i]);
+                string c = Convert.ToString(secreword[i]).ToLower();
                 if (letterGuessed.Contains(c))
                 {
                     word = true;
                 }
                 else
                 {
-                    return word = false;
+                    return false;
                 }
             }
             return word;
@@ -115,7 +170,7 @@ namespace Hangman
             for (int i = 0; i < secretword.Length; i++)
             {
                 string c = Convert.ToString(secretword[i]);
-                if (letterGuessed.Contains(c))
+                if (letterGuessed.Contains(c.ToLower()))
                 {
                     correctletters += c;
                 }
@@ -126,31 +181,7 @@ namespace Hangman
             }
             return correctletters;
         }
-        static void PrintLeftAlphabet(string letters)
-        {
-            List<string> alphabet = new List<string>();
-            for (int i = 1; i <= 26; i++)
-            {
-                char alpha = Convert.ToChar(i + 96);
-                alphabet.Add(Convert.ToString(alpha));
-            }
-            int num = 49;
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.WriteLine("Letters Left are :");
-            for (int i = 0; i < num; i++)
-            {
-                if (letters.Contains(letters))
-                {
-                    alphabet.Remove(letters);
-                    num -= 1;
-                }
-                Console.Write("[" + alphabet[i] + "] ");
-            }
-            Console.WriteLine();
-        }
-        
     }
-    
 }
 
 
